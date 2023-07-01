647357249666a071a398c2ba11550f8bd1e1e66f
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-07-01 19:49:47 +0200

    Python OOP

---
+++ Encapsulation/football_team_generator/project/team.py
@@ -0,0 +1,25 @@
+from typing import List
+
+from project.player import Player
+
+
+class Team:
+    def __init__(self, name: str, rating: int):
+        self.__name = name
+        self.__rating = rating
+        self.__players: List[object] = []
+
+    def add_player(self, player: Player):
+        if player in self.__players:
+            return f'Player {player.name} has already joined'
+        else:
+            self.__players.append(player)
+            return f'Player {player.name} joined team {self.__name}'
+
+    def remove_player(self, player_name: str):
+        try:
+            player = [i for i in self.__players if i.name == player_name][0]
+            self.__players.remove(player)
+            return player
+        except IndexError:
+            return f'Player {player_name} not found'
