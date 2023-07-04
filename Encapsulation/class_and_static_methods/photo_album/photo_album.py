32b872d6a9841c588ce4641fa8fa18ea2f5a4349
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-07-04 08:32:19 +0200

    Python OOP

--- Encapsulation/class_and_static_methods/photo_album/photo_album.py
+++ Encapsulation/class_and_static_methods/photo_album/photo_album.py
@@ -4,36 +4,34 @@ from math import ceil
 class PhotoAlbum:
     def __init__(self, pages: int):
         self.pages = pages
-        self.photos = [[] for i in range(self.pages)]
+        self.photos = [[] for i in range(pages)]
 
     @classmethod
     def from_photos_count(cls, photos_count: int):
         return cls(ceil(photos_count / 4))
 
     def add_photo(self, label: str):
-        for i in range(len(self.photos)):
+        for i in range(self.pages):
             if len(self.photos[i]) < 4:
                 self.photos[i].append(label)
 
                 return f'{label} photo added successfully on page {i + 1} slot {len(self.photos[i])}'
         return 'No more free slots'
 
-    def display(self):
+    def display(self) -> str:
         res = ['-' * 11]
 
         for page in self.photos:
-            res.append(('[] ' * len(page)).rsplit())
+            res.append(('[] ' * len(page)).rstrip())
             res.append('-' * 11)
 
-        return '\n'.join(res)
+        return "\n".join(res)
 
 
-album = PhotoAlbum(2)
-print(album.add_photo("baby"))
-print(album.add_photo("first grade"))
-print(album.add_photo("eight grade"))
-print(album.add_photo("party with friends"))
-print(album.photos)
-print(album.add_photo("prom"))
-print(album.add_photo("wedding"))
-print(album.display())
+album = PhotoAlbum(1)
+album.add_photo("baby")
+album.add_photo("first grade")
+album.add_photo("eight grade")
+album.add_photo("party with friends")
+result = album.display().strip()
+print(result)
