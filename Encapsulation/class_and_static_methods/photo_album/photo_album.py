4045c1a3bae15744dc1cd1fd9968c6c51820a35f
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-07-04 06:39:09 +0200

    Python OOP

--- Encapsulation/class_and_static_methods/photo_album/photo_album.py
+++ Encapsulation/class_and_static_methods/photo_album/photo_album.py
@@ -1,19 +1,35 @@
+import audioop
+
+
 class PhotoAlbum:
     def __init__(self, pages: int):
         self.pages = pages
-        self.photos = []
+        self.photos = [[] for i in range(self.pages)]
 
     @classmethod
     def from_photos_count(cls, photos_count: int):
-        cls()
+        return cls(photos_count)
 
     def add_photo(self, label: str):
-        pass
+        if len(self.photos) < 4:
+            self.photos.append(label)
+            return f'{label} photo added successfully on page  slot '
+        return 'No more free slots'
+
 
     def display(self):
         pass
 
 
-album = PhotoAlbum(2)
+a = PhotoAlbum(4)
+print(a.photos)
 
-print(album.add_photo("baby"))
+# album = PhotoAlbum(2)
+# print(album.add_photo("baby"))
+# print(album.add_photo("first grade"))
+# print(album.add_photo("eight grade"))
+# print(album.add_photo("party with friends"))
+# print(album.photos)
+# print(album.add_photo("prom"))
+# print(album.add_photo("wedding"))
+# print(album.display())
