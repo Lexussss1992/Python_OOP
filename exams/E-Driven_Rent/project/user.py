6705afae0c13ac1909c9751e8eb2bfb869014f61
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-07-13 07:34:23 +0200

    Python OOP

--- exams/E-Driven_Rent/project/user.py
+++ exams/E-Driven_Rent/project/user.py
@@ -4,7 +4,7 @@ class User:
         self.last_name = last_name
         self.driving_license_number = driving_license_number
         self.rating = 0
-        # self.is_blocked = False
+        self.is_blocked = False
 
     @property
     def first_name(self):
@@ -50,10 +50,18 @@ class User:
     def rating(self, value):
         self._rating = value
 
-# driving_license_number: str, rating: float?
+    def increase_rating(self):
+        if self.rating + 0.5 > 10:
+            self.rating = 10
+        else:
+            self.rating += 0.5
 
+    def decrease_rating(self):
+        if self.rating - 2.0 < 0:
+            self.rating = 0
+            self.is_blocked = True
+        else:
+            self.rating -= 2.0
 
-a = User('Ivo', 'gsdf', 'dsadasdad')
-print(a.last_name)
-print(a.driving_license_number)
-print(a.rating)
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} Driving license: {self.driving_license_number} Rating: {self.rating}'
