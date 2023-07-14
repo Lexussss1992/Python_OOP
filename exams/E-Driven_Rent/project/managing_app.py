59ad213e5c71dced2e6db289f57d10fe00f38ba9
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-07-14 21:03:54 +0200

    Python OOP

--- exams/E-Driven_Rent/project/managing_app.py
+++ exams/E-Driven_Rent/project/managing_app.py
@@ -1,4 +1,6 @@
 from project.user import User
+from project.vehicles.cargo_van import CargoVan
+from project.vehicles.passenger_car import PassengerCar
 
 
 class ManagingApp:
@@ -16,11 +18,44 @@ class ManagingApp:
 
         return f'{driving_license_number} has already been registered to our platform.'
 
-    def upload_vehicle(self):
+    def upload_vehicle(self, vehicle_type: str, brand: str, model: str, license_plate_number: str):
+        if vehicle_type not in ('PassengerCar', 'CargoVan'):
+            return f'Vehicle type {vehicle_type} is inaccessible.'
+
+        try:
+            [v for v in self.vehicles if v.license_plate_number == license_plate_number][0]
+        except IndexError:
+            if vehicle_type == 'PassengerCar':
+                self.vehicles.append(PassengerCar(brand, model, license_plate_number))
+                return f'{brand} {model} was successfully uploaded with LPN-{license_plate_number}.'
+            elif vehicle_type == 'CargoVan':
+                self.vehicles.append(CargoVan(brand, model, license_plate_number))
+                return f'{brand} {model} was successfully uploaded with LPN-{license_plate_number}.'
+
+        return f'{license_plate_number} belongs to another vehicle.'
+
+    def allow_route(self, start_point: str, end_point: str, length: float):
+
 
 
 app = ManagingApp()
 
 print(app.register_user( 'Tisha', 'Reenie', '7246506' ))
+
 print(app.register_user( 'Bernard', 'Remy', 'CDYHVSR68661'))
+
 print(app.register_user( 'Mack', 'Cindi', '7246506'))
+
+print(app.upload_vehicle('PassengerCar', 'Chevrolet', 'Volt', 'CWP8032'))
+
+print(app.upload_vehicle( 'PassengerCar', 'Volkswagen', 'e-Up!', 'COUN199728'))
+
+print(app.upload_vehicle('PassengerCar', 'Mercedes-Benz', 'EQS', '5UNM315'))
+
+print(app.upload_vehicle('CargoVan', 'Ford', 'e-Transit', '726QOA'))
+
+print(app.upload_vehicle('CargoVan', 'BrightDrop', 'Zevo400', 'SC39690'))
+
+print(app.upload_vehicle('EcoTruck', 'Mercedes-Benz', 'eActros', 'SC39690'))
+
+print(app.upload_vehicle('PassengerCar', 'Tesla', 'CyberTruck', '726QOA'))
