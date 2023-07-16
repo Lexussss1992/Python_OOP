f876d252dcdc93f7a1eb117c090d3dbac77da46f
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-07-16 10:15:27 +0200

    Python OOP

--- SOLID/exercise-resources/workers.py
+++ SOLID/exercise-resources/workers.py
@@ -1,13 +1,5 @@
-class Worker:
-
-    def work(self):
-        print("I'm working!!")
-
 
 class Manager:
-
-    def __init__(self):
-        self.worker = None
 
     def set_worker(self, worker):
         assert isinstance(worker, Worker), '`worker` must be of type {}'.format(Worker)
@@ -18,7 +10,14 @@ class Manager:
         if self.worker is not None:
             self.worker.work()
 
-class SuperWorker:
+
+class Worker:
+
+    def work(self):
+        print("I'm working!!")
+
+
+class SuperWorker(Worker):
 
     def work(self):
         print("I work very hard!!!")
@@ -29,9 +28,9 @@ worker = Worker()
 manager = Manager()
 manager.set_worker(worker)
 manager.manage()
-
 super_worker = SuperWorker()
 try:
-    manager.set_worker(super_worker)
+ manager.set_worker(super_worker)
+ manager.manage()
 except AssertionError:
-    print("manager fails to support super_worker....")
+ print("manager fails to support super_worker....")
