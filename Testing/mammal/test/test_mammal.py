182e5052e77d513f693a31569b411e0a0aff4e29
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-07-22 08:06:43 +0200

    Python OOP

--- Testing/mammal/test/test_mammal.py
+++ Testing/mammal/test/test_mammal.py
@@ -1,8 +1,36 @@
-from unittest import TestCase
+import unittest
 
 from project.mammal import Mammal
 
 
-class TestMammalClass(TestCase):
-    def setUp(self) -> None:
-        mammal == Mammal('Ivo', 'human', 'speak')
+class TestMammalClass(unittest.TestCase):
+    def test_init(self):
+
+        #Act
+
+        mammal = Mammal('Ivo', 'human', 'speak')
+
+        #Assert
+
+        self.assertEqual('Ivo', mammal.name)
+        self.assertEqual('human', mammal.type)
+        self.assertEqual('speak', mammal.sound)
+
+    def test_making_sound(self):
+        mammal = Mammal('Ivo', 'human', 'speak')
+
+        self.assertEqual('Ivo makes speak', mammal.make_sound())
+
+    def test_info(self):
+        mammal = Mammal('Ivo', 'human', 'speak')
+
+        self.assertEqual('Ivo is of type human', mammal.info())
+
+    def test_get_kingdom(self):
+        mammal = Mammal('Ivo', 'human', 'speak')
+
+        self.assertEqual('animals', mammal.get_kingdom())
+
+
+if __name__ == '__main__':
+    unittest.main()
