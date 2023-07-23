378b31bdaad822fd9e20403c04f84a363e06dd45
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-07-23 09:04:28 +0200

    Python OOP

--- Testing/vehicle/test/test_vehicle.py
+++ Testing/vehicle/test/test_vehicle.py
@@ -25,10 +25,59 @@ class TestVehicle(unittest.TestCase):
         expected_fuel = 2.1 - (1 * 1.25)
         self.assertEqual(expected_fuel, vehicle.fuel)
 
+    def test_drive_without_enough_fuel(self):
+        vehicle = Vehicle(2.1, 3.1)
+        self.assertEqual(2.1, vehicle.fuel)
+        self.assertEqual(1.25, vehicle.fuel_consumption)
+
+        with self.assertRaises(Exception) as ex:
+            vehicle.drive(10)
+
+        self.assertEqual('Not enough fuel', ex.exception.args[0])
+
+    def test_drive_with_equal_fuel(self):
+        # Arrange
+        vehicle = Vehicle(2.1, 3.1)
+        self.assertEqual(2.1, vehicle.fuel)
+        self.assertEqual(1.25, vehicle.fuel_consumption)
+
+        # Act
+        vehicle.drive(1.68)
+
+        # Arrange
+        expected_fuel = 2.1 - (1.68 * 1.25)
+        self.assertEqual(expected_fuel, vehicle.fuel)
+
+    def test_refuel_with_enough_capacity(self):
+        # Arrange
+        vehicle = Vehicle(2.1, 3.1)
+        self.assertEqual(2.1, vehicle.fuel)
+        self.assertEqual(1.25, vehicle.fuel_consumption)
+
+        # Act
+        vehicle.refuel(0)
+
+        # Arrange
+        expected_fuel = 2.1 + 0
+        self.assertEqual(expected_fuel, vehicle.capacity)
+
+    def test_refuel_without_enough_capacity(self):
+        vehicle = Vehicle(2.1, 3.1)
+        self.assertEqual(2.1, vehicle.fuel)
+        self.assertEqual(1.25, vehicle.fuel_consumption)
+
+        with self.assertRaises(Exception) as ex:
+            vehicle.refuel(1)
+
+        self.assertEqual('Too much fuel', ex.exception.args[0])
+
+
     def test_string(self):
         vehicle = Vehicle(2.1, 3.1)
+        result = str(vehicle)
+        expected = 'The vehicle has 3.1 horse power with 2.1 fuel left and 1.25 fuel consumption'
 
-        self.assertEqual('The vehicle has 3.1 horse power with 2.1 fuel left and 1.25 fuel consumption', vehicle.__str__())
+        self.assertEqual(expected, result)
 
 
 if __name__ == '__main__':
