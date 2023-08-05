484f8ae66fbdce6f820daa998d45ed03f3a5ad75
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-08-05 13:20:05 +0200

    Python OOP

---
+++ exams/August 2023/project/loans/mortgage_loan.py
@@ -0,0 +1,9 @@
+from project.loans.base_loan import BaseLoan
+
+
+class MortgageLoan(BaseLoan):
+    def __init__(self, interest_rate: float = 3.5, amount: float = 50000):
+        super().__init__(interest_rate, amount)
+
+    def increase_interest_rate(self):
+        self.interest_rate += 0.5
---
+++ exams/August 2023/project/loans/student_loan.py
@@ -0,0 +1,9 @@
+from project.loans.base_loan import BaseLoan
+
+
+class StudentLoan(BaseLoan):
+    def __init__(self, interest_rate: float = 1.5, amount: float = 2000):
+        super().__init__(interest_rate, amount)
+
+    def increase_interest_rate(self):
+        self.interest_rate += 0.2
